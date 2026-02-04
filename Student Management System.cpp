ff220658798e74a46ee149964d4044d43e50a961
mabdulrehman-CS <abdulrehmanmuhammad524@gmail.com>
2026-02-04 15:11:41 +0500

    Update Student Management System

--- Student Management System.cpp
+++ Student Management System.cpp
@@ -9,3 +9,39 @@ string subject1;
 string subject2;
 string subject3;
 int j;
+struct info {
+    string name;
+    string reg;
+    string dept;
+    string section;
+};
+
+struct sub1 {
+    float quiz;
+    float assignments;
+    float mids;
+    float terminal;
+    struct info Add1;
+};
+
+sub1 sb1[m_s];
+
+struct sub2 {
+    float quiz;
+    float assignments;
+    float mids;
+    float terminal;
+    struct info Add2;
+};
+
+sub2 sb2[m_s];
+
+struct sub3 {
+    float quiz;
+    float assignments;
+    float mids;
+    float terminal;
+    struct info Add3;
+};
+
+sub3 sb3[m_s];
