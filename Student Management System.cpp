69a570fdd3b4526810f64e75226888cd44a32e0a
mabdulrehman-CS <abdulrehmanmuhammad524@gmail.com>
2026-02-06 17:28:21 +0500

    Update Student Management System

--- Student Management System.cpp
+++ Student Management System.cpp
@@ -114,3 +114,166 @@ int main(){
 
     return 0;
 }
+
+void enter(){
+	int choice;
+	if(total==0)
+	{
+	cout<<"How many students you want to add:  ";
+	cin>>choice;
+	cin.ignore();
+	cout<<"Enter the name of subject 1: ";
+	getline(cin,subject1);
+	cout<<"Enter the name of subject 2: ";
+	getline(cin,subject2);
+	cout<<"Enter the name of subject 3: ";
+	getline(cin,subject3);
+	total+=choice;
+	for(int i=0;i<choice;i++)
+	{
+		cout<<"Enter data of Student"<<i+1<<":"<<endl<<endl;
+		cout<<"Enter the Name: ";
+		getline(cin,sb1[i].Add1.name);
+		cout<<"Enter the Registration No: ";
+		getline(cin,sb1[i].Add1.reg);
+		cout<<"Enter the Department: ";
+		getline(cin,sb1[i].Add1.dept);
+		cout<<"Enter the Section: ";
+		getline(cin,sb1[i].Add1.section);
+		cout<<"Enter Marks Of Subject "<<subject1<<" : "<<endl<<endl;
+	do
+		 {cout<<"Enter the Quiz marks[Between 0 to 15]: ";
+		 cin>>sb1[i].quiz;}
+		while(sb1[i].quiz>15||sb1[i].quiz<0);
+		
+		do
+		{cout<<"Enter the Assignment marks:[Between 0 to 10] ";
+		cin>>sb1[i].assignments;}
+		while(sb1[i].assignments>10||sb1[i].assignments<0);
+		do{
+		cout<<"Enter the Midterm marks: ";
+		cin>>sb1[i].mids;}
+		while(sb1[i].mids>25||sb1[i].mids<0);
+		do{
+		cout<<"Enter the Terminal marks: ";
+		cin>>sb1[i].terminal;}
+		while(sb1[i].terminal>50||sb3[i].terminal<0);
+		cin.ignore();
+
+		cout<<"Enter Marks Of Subject "<<subject2<<" : "<<endl<<endl;
+		do
+		 {cout<<"Enter the Quiz marks[Between 0 to 15]: ";
+		 cin>>sb2[i].quiz;}
+		while(sb2[i].quiz>15||sb2[i].quiz<0);
+		
+		do
+		{cout<<"Enter the Assignment marks:[Between 0 to 10] ";
+		cin>>sb2[i].assignments;}
+		while(sb2[i].assignments>10||sb2[i].assignments<0);
+		do{
+		cout<<"Enter the Midterm marks: ";
+		cin>>sb2[i].mids;}
+		while(sb2[i].mids>25||sb2[i].mids<0);
+		do{
+		cout<<"Enter the Terminal marks: ";
+		cin>>sb2[i].terminal;}
+		while(sb2[i].terminal>50||sb2[i].terminal<0);
+		cin.ignore();
+		cout<<"Enter Marks Of Subject "<<subject3<<" : "<<endl<<endl;
+		
+		do
+		 {cout<<"Enter the Quiz marks[Between 0 to 15]: ";
+		 cin>>sb3[i].quiz;}
+		while(sb3[i].quiz>15||sb3[i].quiz<0);
+		do
+		{cout<<"Enter the Assignment marks:[Between 0 to 10] ";
+		cin>>sb3[i].assignments;}
+		while(sb3[i].assignments>10||sb3[i].assignments<0);
+		do{
+		cout<<"Enter the Midterm marks: ";
+		cin>>sb3[i].mids;}
+		while(sb3[i].mids>25||sb3[i].mids<0);
+		do{
+		cout<<"Enter the Terminal marks: ";
+		cin>>sb3[i].terminal;}
+		while(sb3[i].terminal>50||sb3[i].terminal<0);
+		cin.ignore();
+	}
+	}
+else{
+    cout<<"How many students you want to add:  ";
+	cin>>choice;
+	cin.ignore();
+	for(int i=total;i<total+choice;i++)
+	{
+		cout<<"Enter data of Student"<<i+1<<":"<<endl<<endl;
+		cout<<"Enter the Name: ";
+		getline(cin,sb1[i].Add1.name);
+		cout<<"Enter the Registration No: ";
+		getline(cin,sb1[i].Add1.reg);
+		cout<<"Enter the Department: ";
+		getline(cin,sb1[i].Add1.dept);
+		cout<<"Enter the Section: ";
+		getline(cin,sb1[i].Add1.section);
+		cout<<"Enter Marks Of Subject "<<subject1<<" : "<<endl<<endl;
+	do
+		 {cout<<"Enter the Quiz marks[Between 0 to 15]: ";
+		 cin>>sb1[i].quiz;}
+		while(sb1[i].quiz>15||sb1[i].quiz<0);
+		
+		do
+		{cout<<"Enter the Assignment marks:[Between 0 to 10] ";
+		cin>>sb1[i].assignments;}
+		while(sb1[i].assignments>10||sb1[i].assignments<0);
+		do{
+		cout<<"Enter the Midterm marks: ";
+		cin>>sb1[i].mids;}
+		while(sb1[i].mids>25||sb1[i].mids<0);
+		do{
+		cout<<"Enter the Terminal marks: ";
+		cin>>sb1[i].terminal;}
+		while(sb1[i].terminal>50||sb3[i].terminal<0);
+		cin.ignore();
+
+		cout<<"Enter Marks Of Subject "<<subject2<<" : "<<endl<<endl;
+		do
+		 {cout<<"Enter the Quiz marks[Between 0 to 15]: ";
+		 cin>>sb2[i].quiz;}
+		while(sb2[i].quiz>15||sb2[i].quiz<0);
+		
+		do
+		{cout<<"Enter the Assignment marks:[Between 0 to 10] ";
+		cin>>sb2[i].assignments;}
+		while(sb2[i].assignments>10||sb2[i].assignments<0);
+		do{
+		cout<<"Enter the Midterm marks: ";
+		cin>>sb2[i].mids;}
+		while(sb2[i].mids>25||sb2[i].mids<0);
+		do{
+		cout<<"Enter the Terminal marks: ";
+		cin>>sb2[i].terminal;}
+		while(sb2[i].terminal>50||sb2[i].terminal<0);
+		cin.ignore();
+		cout<<"Enter Marks Of Subject "<<subject3<<" : "<<endl<<endl;
+		
+		do
+		 {cout<<"Enter the Quiz marks[Between 0 to 15]: ";
+		 cin>>sb3[i].quiz;}
+		while(sb3[i].quiz>15||sb3[i].quiz<0);
+		do
+		{cout<<"Enter the Assignment marks:[Between 0 to 10] ";
+		cin>>sb3[i].assignments;}
+		while(sb3[i].assignments>10||sb3[i].assignments<0);
+		do{
+		cout<<"Enter the Midterm marks: ";
+		cin>>sb3[i].mids;}
+		while(sb3[i].mids>25||sb3[i].mids<0);
+		do{
+		cout<<"Enter the Terminal marks: ";
+		cin>>sb3[i].terminal;}
+		while(sb3[i].terminal>50||sb3[i].terminal<0);
+		cin.ignore();
+	}
+	total+=choice;
+}
+}
