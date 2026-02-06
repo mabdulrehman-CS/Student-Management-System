12e9ba76ed5b7134108266b59eff225a5daaf037
mabdulrehman-CS <abdulrehmanmuhammad524@gmail.com>
2026-02-06 17:18:05 +0500

    Update Student Management System

--- Student Management System.cpp
+++ Student Management System.cpp
@@ -57,6 +57,60 @@ void deleteAllRecords();
 void deleteRecord();
 
 int main(){
+    loadData();
 
-    
+    char value;
+    while (true) {
+        cout << "----------------------------------------------------------------------------------------" << endl;
+        cout << "\t\tEnter an option\t\t" << endl;
+        cout << "----------------------------------------------------------------------------------------" << endl;
+        cout << "->Press 1 to Enter Data: " << endl;
+        cout << "->Press 2 to Show Data: " << endl;
+        cout << "->Press 3 to Search Data: " << endl;
+        cout << "->Press 4 to Update Data: " << endl;
+        cout << "->Press 5 to Find GPA:" << endl;
+        cout << "->Press 6 to Delete All Records:" << endl;
+        cout << "->Press 7 to Delete a single Record:" << endl;
+        cout << "->Press 0 to exit from program"<<endl;
+
+        cout << endl;
+        cout << "Enter the value: ";
+        cin >> value;
+        cin.ignore();
+
+        switch (value) {
+            case '1':
+                enter();
+                saveData();
+                break;
+            case '2':
+                show();
+                break;
+            case '3':
+                search();
+                break;
+            case '4':
+                update();
+                saveData();
+                break;
+            case '5':
+                gpa_finder();
+                break;
+            case '6':
+                deleteAllRecords();
+                saveData();
+                break;
+            case '7':
+                deleteRecord();
+                saveData();
+                break;
+            case('0'):
+            	return false;
+            default:
+                cout << "Invalid input" << endl;
+                break;
+        }
+    }
+
+    return 0;
 }
